f8bef4e5bd6d52aa89c5d89cc8216259ff2ede5a
Christopher Param <cparam2@huskers.unl.edu>
2022-08-25 20:38:09 +0000

    fixed standard library bug

--- hello.c
+++ hello.c
@@ -2,10 +2,13 @@
  * Basic Hello World Program
  */
 
+#include <stdlib.h>
+#include <stdio.h>
+
 int main(int argc, char **argv) {
     printf("Hello, I'm Chrisopher Param!\n");
     printf("Major is Electrical Engineering!\n");
 
     return 0;
-    t
+    
 }
