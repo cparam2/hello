ff8d8394462011b554b9c3358724d32f384498f6
Christopher Param <cparam2@huskers.unl.edu>
2022-08-25 20:31:31 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Basic Hello World Program
+ */
+
+int main(int argc, char **argv) {
+    printf("Hello, I'm Chrisopher Param!\n");
+    printf("Major is Electrical Engineering!\n");
+
+    return 0;
+    t
+}
