2af76f4783084ccc1e36c60582a5b0c0f63ddbbd
Patrick <pgurney2@huskers.unl.edu>
2022-08-29 22:15:40 +0000

    added a  minor for Chris

--- hello.c
+++ hello.c
@@ -9,7 +9,7 @@ int main(int argc, char **argv) {
     printf("Hello, I'm Chrisopher Param!\n");
     printf("Hello, I'm Patrick Gurney!\n")
     printf("Our Majors are Electrical Engineering and Computer Engineering\n");
-    printf("Chris' minor  is Math\n")
+    printf("Chris' minor is in Math\n")
 
     return 0;
 
